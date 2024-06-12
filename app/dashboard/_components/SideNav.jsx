f5fa5057718e6c69a611d8c3f929882b801df89f
Shivam <prajapatishivam11203@gmail.com>
2024-06-12 16:34:06 +0530

    feat: added side navigation on dashboard

---
+++ app/dashboard/_components/SideNav.jsx
@@ -0,0 +1,70 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Progress } from '@/components/ui/progress';
+import { LibraryBig, LineChart, MessageSquare, Shield } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import React, { useEffect } from 'react';
+
+function SideNav() {
+  const menuList = [
+    {
+      id: 1,
+      name: 'My Forms',
+      icon: LibraryBig,
+      path: '/dashboard',
+    },
+    {
+      id: 2,
+      name: 'Responses',
+      icon: MessageSquare,
+      path: '/dashboard/responses',
+    },
+    {
+      id: 3,
+      name: 'Analytics',
+      icon: LineChart,
+      path: '/dashboard/analytics',
+    },
+    {
+      id: 4,
+      name: 'Upgrade',
+      icon: Shield,
+      path: '/dashboard/upgrade',
+    },
+  ];
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, [path]);
+  return (
+    <div className='border-2 shadow-md h-screen'>
+      <div className='p-4'>
+        {menuList.map((menu) => (
+          <div
+            key={menu.id}
+            className={`p-3 mb-3 rounded-lg flex items-center gap-4 hover:bg-primary hover:text-white cursor-pointer transition-colors duration-300 ease-in-out text-sm font-semibold leading-5 hover:shadow-md ${
+              path == menu.path && 'bg-primary text-white font-bold'
+            }`}
+          >
+            <menu.icon size='24' />
+            <span>{menu.name}</span>
+          </div>
+        ))}
+      </div>
+      <div className='fixed bottom-0 p-4 w-64'>
+        <Button className='w-full'>+ Create Form</Button>
+        <div className='my-4'>
+          <Progress value={70} />
+          <p className='mt-2 text-gray-600 text-sm'>
+            <strong>2</strong> out of <strong>3</strong> forms created
+          </p>
+          <p className='text-sm text-gray-600 mt-2'>
+            <strong>Upgrade</strong> to create unlimited forms
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideNav;
---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+'use client';
+import { SignedIn } from '@clerk/nextjs';
+import SideNav from './_components/SideNav';
+
+function DashboardLayout({ children }) {
+  return (
+    <SignedIn>
+      <div>
+        <div className='md:w-64 fixed'>
+          <SideNav />
+        </div>
+        <div className='md:ml-64'>{children}</div>
+      </div>
+    </SignedIn>
+  );
+}
+
+export default DashboardLayout;
